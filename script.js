7e1243f2da943a8051c6164d9c4578f341c67985
andersonsouzamoreira <147444027+andersonsouzamoreira@users.noreply.github.com>
2025-04-01 14:58:16 -0300

    01/04

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesDeAcessibilidade =document.getElementById('opcoes-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
     botaoDeAcessibilidade.addEventListener('click', function(){
        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function(){
+    aumentaFonteBotao.addEventListener('click',function(){
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
