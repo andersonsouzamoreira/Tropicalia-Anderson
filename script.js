cce9fca0535580802545f7d7e0928135a525d089
andersonsouzamoreira <147444027+andersonsouzamoreira@users.noreply.github.com>
2025-04-22 14:26:07 -0300

    22/04

--- script.js
+++ script.js
@@ -34,3 +34,8 @@ botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
 
 
 });
+
+ScrollReveal().reveal('#inicio', {delay: 500});
+ScrollReveal().reveal('#galeria', {delay: 500});
+ScrollReveal().reveal('#tropicalia', {delay: 500});
+ScrollReveal().reveal('#contato', {delay: 500});
